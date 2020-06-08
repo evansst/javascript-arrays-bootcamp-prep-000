26d0c40db37973d0f60eaab47bb70a734b615b78
thoughtful-bundle-9618 <evansst@icloud.com>
2020-06-08 05:28:48 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,35 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'];
+  
+  function addElementToBeginningOfArray(b,a) {
+    var array = [a,...b]
+    return array;
+  }
+  
+  function destructivelyAddElementToBeginningOfArray(b,a) {
+    b.unshift(a)
+    return b;
+  }
+  
+  function addElementToEndOfArray(a,b) {
+    var array = [a,...b]
+    return array;
+  }
+  
+  function destructivelyAddElementToEndOfArray(a,b) {
+    a.push(b)
+    return a;
+  }
+  
+  function accessElementInArray () {
+    
+  }
+  
+  function destructivelyRemoveElementFromBeginningOfArray() {
+  
+  }
+  }
+  
